33aea0a80d50d49b2981282cb7c683a8a597a916
xueming998 <pang-xue-ming@hotmail.com>
2022-11-16 05:48:42 -0800

    “ip”

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,8 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://localhost";
+	//String url = "http://localhost";
+	String url = "192.168.199.154";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
